e0d642608537e03e6f164ada3f83d75a7a35455e
Gustavo Gonzaga <Gustavogonzaga.gg243@gmail.com>
2024-09-22 16:19:29 -0300

    fix: static status route

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import database from '~/infra/database';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const updated_at = new Date().toISOString();
 
