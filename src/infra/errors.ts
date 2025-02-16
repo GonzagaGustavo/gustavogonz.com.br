6a7004c15f7554424c02d9e56b5ea1f80bf50b1a
Gustavo Gonzaga <Gustavogonzaga.gg243@gmail.com>
2025-02-16 15:07:15 -0300

    feat: add `MethodNotAllowedError` and `ServiceError`

--- src/infra/errors.ts
+++ src/infra/errors.ts
@@ -1,13 +1,52 @@
 export class InternalServerError extends Error {
   action = 'Contact Support';
-  statusCode = 500;
+  statusCode: number;
 
-  constructor({ cause }: { cause: unknown }) {
+  constructor({ cause, statusCode }: { cause: unknown; statusCode?: number }) {
     super('An unexpected internal error occurred', { cause });
     this.name = 'InternalServerError';
+    this.statusCode = statusCode || 500;
   }
 
-  toJson() {
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  action = 'Check if the service is available.';
+  statusCode = 503;
+
+  constructor({ cause, message }: { cause: unknown; message?: string }) {
+    super(message || 'Service currently unavailable.', { cause });
+    this.name = 'ServiceError';
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  action = 'Check if the http method is valid for this endpoint.';
+  statusCode = 405;
+
+  constructor() {
+    super('Method not allowed for this endpoint.');
+    this.name = 'MethodNotAllowedError';
+  }
+
+  toJSON() {
     return {
       name: this.name,
       message: this.message,
