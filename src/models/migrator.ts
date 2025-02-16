63f4324bb5f1c018891a1cfd1535d66331fceafd
Gustavo Gonzaga <Gustavogonzaga.gg243@gmail.com>
2025-02-16 16:30:25 -0300

    feat: create `migrator` model

---
+++ src/models/migrator.ts
@@ -0,0 +1,62 @@
+import migrationRunner, { RunnerOption } from 'node-pg-migrate';
+import { resolve } from 'node:path';
+import { Client } from 'pg';
+
+import database from '~/infra/database';
+import { ServiceError } from '~/infra/errors';
+
+const defaultMigrationOptions: Omit<RunnerOption, 'dbClient'> = {
+  dryRun: true,
+  dir: resolve('src', 'infra', 'migrations'),
+  direction: 'up',
+  verbose: true,
+  migrationsTable: 'pgmigrations',
+};
+
+async function listPendingMigrations() {
+  let dbClient: Client | undefined;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient: Client | undefined;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } catch (err) {
+    const serviceErrorObject = new ServiceError({
+      cause: err,
+      message: 'Error runing migrations.',
+    });
+    throw serviceErrorObject;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
