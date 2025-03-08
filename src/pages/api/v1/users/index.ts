2cbc5edeba6661599f9ee3220afd89cf7ac47171
Gustavo Gonzaga <Gustavogonzaga.gg243@gmail.com>
2025-03-07 22:20:24 -0300

    feat: create `user` model and `api/v1/users` endpoint

---
+++ src/pages/api/v1/users/index.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createRouter } from 'next-connect';
+
+import controller from '~/infra/controller';
+import user from '~/models/user';
+
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function postHandler(req: NextApiRequest, res: NextApiResponse) {
+  const userInputValues = req.body;
+
+  const newUser = await user.create(userInputValues);
+
+  return res.status(201).json(newUser);
+}
